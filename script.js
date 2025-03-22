9a944ea7e2f1092f3088ec59defe0df3149e7184
nibirroy007 <nibirroy007@gmail.com>
2025-03-22 22:27:39 +0600

    5th

---
+++ script.js
@@ -0,0 +1,11 @@
+function showAlert() {
+    alert("Thanks for visiting Home Plant Shop! Explore our plants and bring nature home.");
+}
+
+function buyNow() {
+    alert("Thank you for your interest! Our team will contact you shortly.");
+}
+
+function sendMessage() {
+    alert("Thank you! Your message has been sent. We will contact you soon.");
+}
